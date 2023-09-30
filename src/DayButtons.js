1c3965a0dfdc1508e7e4902ba254684b3ddf6398
mateusz <mat.domian@gmail.com>
2023-09-30 12:20:26 +0200

    Added render popup with content based on which button was pressed

--- src/DayButtons.js
+++ src/DayButtons.js
@@ -1,52 +1,57 @@
+import React, { useState } from 'react';
 
-function DayButton({ value }){
-    return <button className="day-button">{value}</button>; 
+
+const days = [{ dayNo: "1" },
+{ dayNo: "2" },
+{ dayNo: "3" },
+{ dayNo: "4" },
+{ dayNo: "5" },
+{ dayNo: "6" },
+{ dayNo: "7" },
+{ dayNo: "8" },
+{ dayNo: "9" },
+{ dayNo: "10" },
+{ dayNo: "11" },
+{ dayNo: "12" },
+{ dayNo: "13" },
+{ dayNo: "14" },
+{ dayNo: "15" },
+{ dayNo: "16" },
+{ dayNo: "17" },
+{ dayNo: "18" },
+{ dayNo: "19" },
+{ dayNo: "20" },
+{ dayNo: "21" },
+{ dayNo: "22" },
+{ dayNo: "23" },
+{ dayNo: "24" }
+];
+
+
+function DayButton() {
+
+// added OnClick event handler to each button
+// button passes the dayNo value as a parameter to the handleButtonClick function where we update the state of DayContent
+//Finally we conditionally render a window displaying content based on the DayContent state 
+
+
+const [DayContent, setDayContent] = useState(false);
+
+const handleButtonClick = (dayContentNo) => {
+    setDayContent(dayContentNo);
 }
 
-export default function CallendarPage(){
     return (
-    <>
-    <div className="calendar-row">
-     <DayButton value="1" />
-     <DayButton value="2" />
-     <DayButton value="3" />
-     <DayButton value="4" />     
-     </div>
-
-    <div className="calendar-row">
-     <DayButton value="5" />
-     <DayButton value="6" />
-     <DayButton value="7" />
-     <DayButton value="8" />     
-     </div>
-
-    <div className="calendar-row">
-     <DayButton value="9" />
-     <DayButton value="10" />
-     <DayButton value="11" />
-     <DayButton value="12" />     
-     </div>
-
-    <div className="calendar-row">
-     <DayButton value="13" />
-     <DayButton value="14" />
-     <DayButton value="15" />
-     <DayButton value="16" />     
-     </div>
-
-    <div className="calendar-row">
-     <DayButton value="17" />
-     <DayButton value="18" />
-     <DayButton value="19" />
-     <DayButton value="20" />     
-     </div>
-
-    <div className="calendar-row">
-     <DayButton value="21" />
-     <DayButton value="22" />
-     <DayButton value="23" />
-     <DayButton value="24" />     
-     </div>
-    </>
+      <div>
+        {days.map((el, key) => {
+          return <button onClick={() => handleButtonClick(el.dayNo)} key={key}>{el.dayNo}</button>;
+        })}
+        {DayContent && (
+        <div className='popup-content'>{DayContent}</div>
+        )}
+        </div>
     );
-}
+  }
+
+
+export default DayButton;
